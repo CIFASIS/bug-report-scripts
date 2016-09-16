82c4aa3ac17b39a36094132373fb77b6214f5684
fcostantini <franco.costantini20@gmail.com>
2016-09-16 12:00:25 -0300

    general getErrors

--- getErrors.sh
+++ getErrors.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 mkdir reports -p
+command="(command)"
 
 for i in $(ls $1 | grep sh); do
-  echo 'We recently found an invalid memory access parsing and executing fuzzed bash code in Busybox 1.25.0.' > reports/rep.$i
-  echo "We tested this issue on Ubuntu 14.04.5 (x86_64) but other configurations could be affected. Please find attached the full .config file" >> reports/rep.$i
+  echo 'Hello, we recently found (error) parsing and executing fuzzed (format) code in (application).' > reports/rep.$i
+  echo "We tested this issue on (platform) but other configurations could be affected." >> reports/rep.$i
   echo "Technical details about the issue are:" >> reports/rep.$i
   echo "" >> reports/rep.$i
   ./busybox_unstripped sh $1/$i 2>&1 >/dev/null | grep -A1 AddressSanitizer >> reports/rep.$i
   echo "" >> reports/rep.$i
   echo "gdb backtrace is as follows:" >> reports/rep.$i
   echo "" >> reports/rep.$i
-  env -i ASAN_OPTIONS='abort_on_error=1' gdb -batch -ex 'tty /dev/null' -ex run -ex 'bt 25' --args ./busybox_unstripped sh $1/$i 2> /dev/null >> reports/rep.$i
+  env -i ASAN_OPTIONS='abort_on_error=1' gdb -batch -ex 'tty /dev/null' -ex run -ex 'bt 25' --args $command 2> /dev/null >> reports/rep.$i
   echo "" >> reports/rep.$i
   echo "This issue was found using QuickFuzz, the file to reproduce it is attached." >> reports/rep.$i
   echo "Regards." >> reports/rep.$i
